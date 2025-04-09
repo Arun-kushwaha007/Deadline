0032ae3459055b896e4a031ae51c624b9175f794
Arun <arunsk1310@gmail.com>
2025-04-10 00:23:04 +0530

    basic change 2

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#0f172a',
+        accent: '#7c3aed',
+        primary: '#3b82f6'
+      }
+    },
+  },
+  plugins: [react(),tailwindcss()],
+};
+
