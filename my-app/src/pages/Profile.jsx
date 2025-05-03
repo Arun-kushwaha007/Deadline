54aa5a98f5b6f6345917e834d83969af303e5286
Arun <arunsk1310@gmail.com>
2025-05-03 20:23:22 +0530

    profile2

--- my-app/src/pages/Profile.jsx
+++ my-app/src/pages/Profile.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div>
+        <h1>Profile</h1>
+        <p>This is the profile page.</p>
+        <p>Here you can view and edit your profile information.</p>
+        <p>Feel free to customize this page as per your requirements.</p>
+        <p>Have a great day!</p>
+    </div>
   )
 }
 
