0dd0576dd27f7b41c0e2e7455b2629d0f2bf9dbe
Arun <arunsk1310@gmail.com>
2025-05-07 00:02:14 +0530

    hihi

--- my-app/src/pages/Profile.jsx
+++ my-app/src/pages/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
     <div>
         <h1>Profile</h1>
         <p>This is the profile page.</p>
-        <p>Here you can view and edit your profile information.</p>
+        
        
     </div>
   )
