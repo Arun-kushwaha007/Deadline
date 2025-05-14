903a87902253b71bf38f97b0453075f18a0f2027
Arun <arunsk1310@gmail.com>
2025-05-14 19:27:01 +0530

    local login setup done

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -1,32 +1,61 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const storedUser = localStorage.getItem(data.email);
+    if (storedUser) {
+      const parsedUser = JSON.parse(storedUser);
+      if (parsedUser.password === data.password) {
+        console.log(`${parsedUser.name}, you are successfully logged in.`);
+      } else {
+        console.log("Incorrect email or password.");
+      }
+    } else {
+      console.log("User not found. Please check your credentials.");
+    }
+  };
+
   return (
-    <div className="h-screen flex justify-center items-center text-black">
+    <div className="h-screen flex justify-center items-center text-black ">
       <div>
         <p className="text-center w-[30vw] bg-purple-300 py-[2vw] px-[1vw] rounded-t-[10px] text-2xl font-[Verdana]">
           Login Form
         </p>
 
-        <form className="text-center flex flex-col mx-auto w-[30vw] py-[2vw] px-[1vw] bg-pink-200 rounded-b-[10px]">
-          <input
-            type="text"
-            className="border border-gray-400 rounded-[10px] p-[1vw] outline-none m-[5px]"
-            placeholder="Username"
-          />
+        <form
+          className="text-center flex flex-col mx-auto w-[30vw] py-[2vw] px-[1vw] bg-pink-200 rounded-b-[10px]"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <input
             type="email"
-            className="border border-gray-400 rounded-[10px] p-[1vw] outline-none m-[5px]"
+            {...register("email", { required: true })}
             placeholder="Email"
+            className="border border-gray-400 rounded-[10px] p-[1vw] outline-none m-[5px]"
           />
+          {errors.email && (
+            <span className="text-red-600 text-sm mb-2">*Email* is mandatory</span>
+          )}
+
           <input
             type="password"
-            className="border border-gray-400 rounded-[10px] p-[1vw] outline-none m-[5px]"
+            {...register("password", { required: true })}
             placeholder="Password"
+            className="border border-gray-400 rounded-[10px] p-[1vw] outline-none m-[5px]"
           />
+          {errors.password && (
+            <span className="text-red-600 text-sm mb-2">*Password* is mandatory</span>
+          )}
+
           <input
             type="submit"
-            value="Register"
+            value="Login"
             className="rounded-[10px] p-[1vw] m-[5px] cursor-pointer"
             style={{ backgroundColor: "#a1eafb" }}
           />
--- my-app/src/pages/Register.jsx
+++ my-app/src/pages/Register.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+const onSubmit = (data) => {
+    localStorage.setItem(data.email, JSON.stringify({ 
+        name: data.name, password: data.password 
+    }));
+    console.log(JSON.parse(localStorage.getItem(data.email)));
+  };
+
   return (
     <div className="h-screen flex justify-center items-center text-black">
       <div>
@@ -8,22 +22,36 @@ const Register = () => {
           Registration Form
         </p>
 
-        <form className="text-center flex flex-col mx-auto w-[30vw] py-[2vw] px-[1vw] bg-pink-200 rounded-b-[10px]">
+        <form
+          className="text-center flex flex-col mx-auto w-[30vw] py-[2vw] px-[1vw] bg-pink-200 rounded-b-[10px]"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <input
             type="text"
+            {...register("name")}
+            placeholder="Name"
             className="border border-gray-400 rounded-[10px] p-[1vw] outline-none m-[5px]"
-            placeholder="Username"
           />
+
           <input
             type="email"
-            className="border border-gray-400 rounded-[10px] p-[1vw] outline-none m-[5px]"
+            {...register("email", { required: true })}
             placeholder="Email"
+            className="border border-gray-400 rounded-[10px] p-[1vw] outline-none m-[5px]"
           />
+          {errors.email && (
+            <span className="text-red-600 text-sm mb-2">
+              *Email* is mandatory
+            </span>
+          )}
+
           <input
             type="password"
-            className="border border-gray-400 rounded-[10px] p-[1vw] outline-none m-[5px]"
+            {...register("password")}
             placeholder="Password"
+            className="border border-gray-400 rounded-[10px] p-[1vw] outline-none m-[5px]"
           />
+
           <input
             type="submit"
             value="Register"
